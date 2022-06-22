18743708b6864c712d91e36633eef1119adf8875
Bryan Hor Jin Hao <2702bryan@gmail.com>
2022-06-22 12:50:20 +0800

    Added the csv, jupyter and python files

---
+++ Test.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from plotly_calplot import calplot
+
+df = pd.read_csv('CRM-GRN-PO-Better.csv')
+df['CreatedDate'] = pd.to_datetime(df['CreatedDate'])
+df_test = df[["CreatedDate", "PurchaseOrder"]]
+df_group = pd.DataFrame(df_test.groupby(['CreatedDate', 'PurchaseOrder']).size())
+df_group.reset_index(inplace=True)
+df_group.rename(columns = {0: 'Count'}, inplace=True)
+fig = calplot(df_group, x='CreatedDate', y='Count', years_title=True, showscale=True, gap=1)
+fig.show()
