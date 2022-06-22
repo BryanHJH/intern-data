279792d548f13916211aa324ba9ca1db2f29af55
Bryan Hor Jin Hao <2702bryan@gmail.com>
2022-06-22 13:32:25 +0800

    Edited the hover name

--- Test.py
+++ Test.py
@@ -7,5 +7,5 @@ df_test = df[["CreatedDate", "PurchaseOrder"]]
 df_group = pd.DataFrame(df_test.groupby(['CreatedDate', 'PurchaseOrder']).size())
 df_group.reset_index(inplace=True)
 df_group.rename(columns = {0: 'Count'}, inplace=True)
-fig = calplot(df_group, x='CreatedDate', y='Count', years_title=True, showscale=True, gap=1)
-fig.show()
+fig = calplot(df_group, x='CreatedDate', y='Count', name="Purchase Orders", years_title=True, showscale=True, gap=1)
+fig.write_html("index.html")
